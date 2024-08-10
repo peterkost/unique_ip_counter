022e071a5029e9be813b953e897187c42df5804d
peterkost <46546031+peterkost@users.noreply.github.com>
2024-08-10 12:25:21 -0500

    feat: count line number with scanner

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Lightspeed Take Home Assessment
 
 https://github.com/Ecwid/new-job/blob/master/IP-Addr-Counter-GO.md
+
+# Dev Log
+
+## Iteration One - Scanning the input to count the number of lines
+
+The first challenege to takle is reading in the massive input file. With a naive
+file scanner it took about thirteen minutes for the script to count up the number of
+lines. The result was eight billion.
--- uniqueip.go
+++ uniqueip.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+  filePath := os.Args[1]
+  readFile, err := os.Open(filePath)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fileScanner := bufio.NewScanner(readFile)
+  fileScanner.Split(bufio.ScanLines)
+
+  lineCount := 0
+  for fileScanner.Scan() {
+    lineCount++
+    if lineCount % 100000000 == 0 {
+      fmt.Println(lineCount)
+    }
+  }
+
+  readFile.Close()
+
+  fmt.Println("total lines: ", lineCount)
 }
+
+
